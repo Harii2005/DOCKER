541d18f6377d43ba6fe808d7abf0e3ffe1ebaa23
Harii2005 <harikrishnankrkv@gmail.com>
2026-01-07 14:44:53 +0530

    finally

--- server.js
+++ server.js
@@ -5,38 +5,42 @@ const MongoClient = require("mongodb").MongoClient;
 
 const PORT = 3030;
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static("public"));
 
-const MONGO_URL = "mongodb://delta_admin:delta_password@127.0.0.1:27017/";
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+const MONGO_URL = "mongodb://delta_admin:delta_password@localhost:27017";
 const client = new MongoClient(MONGO_URL);
 
-//GET all users
-app.get("/getUsers", async (req, res) => {
-    await client.connect();
-    console.log('Connected successfully to server');
-
-    const db = client.db("my-sample-db");
-    const data = await db.collection('users').find({}).toArray();
-    
-    client.close();
-    res.send(data);
-});
-
-//POST new user
-app.post("/addUser", async (req, res) => {
-    const userObj = req.body;
-    console.log(req.body);
-    await client.connect();
-    console.log('Connected successfully to server');
-
-    const db = client.db("my-sample-db");
-    const data = await db.collection('users').insertOne(userObj);
-    console.log(data);
-    console.log("data inserted in DB");
-    client.close();
-});
-
-
 app.listen(PORT, () => {
-    console.log(`server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
+});
+
+
+// GET all users
+
+app.get("/getUsers", async (req, res) => {
+  await client.connect(MONGO_URL);
+  console.log("Connected successfully to server");
+
+  const db = client.db("my-sample-db");
+  const data = await db.collection("users").find({}).toArray();
+
+  await client.close();
+  res.send(data);
+});
+
+// POST new user
+app.post("/addUser", async (req, res) => {
+  const userObj = req.body;
+
+  await client.connect(MONGO_URL);
+  console.log("Connected successfully to server");
+
+  const db = client.db("my-sample-db");
+  const data = await db.collection("users").insertOne(userObj);
+
+  await client.close();
+  res.send(data);
 });
